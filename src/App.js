e122ab9c501fa29f6920dbe14861bbf884e265ff
Mahamadou Touray <mahamadou12@gmail.com>
2023-07-27 19:12:10 -0400

    seed

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import {useState} from 'react';
 
 import { Routes, Route } from 'react-router-dom';
 
-import NewOrderPage from './pages/NewOrderPage';
-import AuthPage from './pages/AuthPage';
-import OrderHistoryPage from './pages/OrderHistoryPage';
+import NewOrderPage from './pages/NewOrderPage/NewOrderPage';
+import AuthPage from './pages/AuthPage/AuthPage';
+import OrderHistoryPage from './pages/OrderHistoryPage/OrderHistoryPage';
 import NavBar from './components/NavBar';
 
 import { getUser } from './utilities/users-service';
