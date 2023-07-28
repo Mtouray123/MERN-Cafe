2e788e6f73c11b98daf0c1df117fcc014238f405
Mahamadou Touray <mahamadou12@gmail.com>
2023-07-28 08:26:54 -0400

    components

--- src/components/UserLogOut/UserLogOut.js
+++ src/components/UserLogOut/UserLogOut.js
@@ -2,16 +2,21 @@ import styles from './UserLogOut.module.css';
 import { logOut } from '../../utilities/users-service';
 
 export default function UserLogOut({ user, setUser }) {
-function handleLogOut() {
-  logOut();
-  setUser(null);
-}
+  function handleLogOut() {
+    logOut();
+    setUser(null);
+  }
 
-return (
-  <div className={styles.UserLogOut}>
-    <div>{user.name}</div>
-    <div className={styles.email}>{user.email}</div>
-    <button className="btn-sm" onClick={handleLogOut}>LOG OUT</button>
-  </div>
-);
+  // Check if 'user' is available before accessing its properties
+  if (!user) {
+    return null; // Return null or an appropriate loading/placeholder component
+  }
+
+  return (
+    <div className={styles.UserLogOut}>
+      <div>{user.name}</div>
+      <div className={styles.email}>{user.email}</div>
+      <button className="btn-sm" onClick={handleLogOut}>LOG OUT</button>
+    </div>
+  );
 }
